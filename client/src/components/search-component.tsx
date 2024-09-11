0a0d969edc66a702a00f03c3968b931425f81fdf
lennrt jansen <lennartjansen@proton.me>
2024-09-11 11:10:27 +0200

    test

--- client/src/components/search-component.tsx
+++ client/src/components/search-component.tsx
@@ -3,7 +3,22 @@ import styles from "./logo.module.scss";
 
 const SearchInputComponent: FC = (): ReactElement => {
   return (
-    <div className="h-full flex justify-center items-center">
+    <div className="h-full flex justify-center items-center relative">
+      {/* --NOTE: start genre list */}
+      <div className="absolute left-0">
+        <div tabIndex={0} role="button" className="btn m-1">
+          All
+        </div>
+        <ul tabIndex={0} className="bg-base-100 rounded-box">
+          <li>
+            <a>Item 1</a>
+          </li>
+          <li>
+            <a>Item 2</a>
+          </li>
+        </ul>
+      </div>
+      {/* --NOTE: start genre list */}
       <input
         type="text"
         placeholder="Search"
